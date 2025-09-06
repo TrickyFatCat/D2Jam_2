197bbdc0898ab9bb367faab9492f15ef2b1d4091
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-09-06 10:56:04 +0300

    Update Build.cs for the project

--- Source/D2Jam_2/D2Jam_2.Build.cs
+++ Source/D2Jam_2/D2Jam_2.Build.cs
@@ -10,7 +10,7 @@ public class D2Jam_2 : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "TrickyProperties", "TrickyGameplayObjects", "TrickyGameMode" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
