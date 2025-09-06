c480bf60a37b502884bad38a02392331ff4b6347
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-09-06 10:55:29 +0300

    Add `D2JGameMode` class

---
+++ Source/D2Jam_2/Core/D2JGameMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrickyGameModeBase.h"
+#include "D2JGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class D2JAM_2_API AD2JGameMode : public ATrickyGameModeBase
+{
+	GENERATED_BODY()
+};
