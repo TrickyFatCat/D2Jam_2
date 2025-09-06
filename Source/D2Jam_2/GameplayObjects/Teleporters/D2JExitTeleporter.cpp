aabe2b22257d8bb4cbfaa3aca5d4f1f132d38df4
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-09-06 17:56:42 +0300

    Teleporter. Add exit teleporter implementation

---
+++ Source/D2Jam_2/GameplayObjects/Teleporters/D2JExitTeleporter.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "D2JExitTeleporter.h"
+
+#include "D2Jam_2/PlayerCharacter/D2JPlayerInterface.h"
+#include "Kismet/GameplayStatics.h"
+
+
+AD2JExitTeleporter::AD2JExitTeleporter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AD2JExitTeleporter::HandleTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+                                                   AActor* OtherActor,
+                                                   UPrimitiveComponent* OtherComp,
+                                                   int32 OtherBodyIndex,
+                                                   bool bFromSweep,
+                                                   const FHitResult& SweepResult)
+{
+	Super::HandleTriggerBeginOverlap(OverlappedComponent,
+	                                 OtherActor,
+	                                 OtherComp,
+	                                 OtherBodyIndex,
+	                                 bFromSweep,
+	                                 SweepResult);
+	
+	if (!OtherActor->GetClass()->ImplementsInterface(UD2JPlayerInterface::StaticClass()))
+	{
+		return;
+	}
+
+	ID2JPlayerInterface* PlayerInterface = Cast<ID2JPlayerInterface>(OtherActor);
+	PlayerInterface->ToggleInput(false);
+}
+
+void AD2JExitTeleporter::HandleCameraFadeInFinished()
+{
+	Super::HandleCameraFadeInFinished();
+
+	UKismetSystemLibrary::QuitGame(this, nullptr, EQuitPreference::Quit, false);
+}
---
+++ Source/D2Jam_2/GameplayObjects/Teleporters/D2JExitTeleporter.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "D2JTeleporterBase.h"
+#include "D2JExitTeleporter.generated.h"
+
+UCLASS()
+class D2JAM_2_API AD2JExitTeleporter : public AD2JTeleporterBase
+{
+	GENERATED_BODY()
+
+public:
+	AD2JExitTeleporter();
+
+protected:
+	virtual void HandleTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+	                                       AActor* OtherActor,
+	                                       UPrimitiveComponent* OtherComp,
+	                                       int32 OtherBodyIndex,
+	                                       bool bFromSweep,
+	                                       const FHitResult& SweepResult) override;
+	
+	virtual void HandleCameraFadeInFinished() override;
+};
