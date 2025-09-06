de1f497123d1918e8d63f119a8d4e08ba3ed3518
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-09-06 17:56:20 +0300

    Teleporter. Implement level-switching teleportation logic

---
+++ Source/D2Jam_2/GameplayObjects/Teleporters/D2JLevelTeleporter.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "D2JTeleporterBase.h"
+#include "D2JLevelTeleporter.generated.h"
+
+UCLASS()
+class D2JAM_2_API AD2JLevelTeleporter : public AD2JTeleporterBase
+{
+	GENERATED_BODY()
+
+public:
+	AD2JLevelTeleporter();
+
+protected:
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category="Teleporter")
+	TSoftObjectPtr<UWorld> LevelToLoad = nullptr;
+
+	virtual void HandleStateChanged(UGameplayObjectStateControllerComponent* Component,
+	                                EGameplayObjectState NewState,
+	                                bool bChangedImmediately) override;
+
+	virtual void HandleTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+	                                       AActor* OtherActor,
+	                                       UPrimitiveComponent* OtherComp,
+	                                       int32 OtherBodyIndex,
+	                                       bool bFromSweep,
+	                                       const FHitResult& SweepResult) override;
+
+	virtual void HandleCameraFadeInFinished() override;
+};
