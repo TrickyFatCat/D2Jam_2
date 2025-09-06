f447366d388b36d89464a6e75b7b0a18c656539e
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-09-06 12:08:50 +0300

    Update dependencies in Build.cs

--- Source/D2Jam_2/D2Jam_2.Build.cs
+++ Source/D2Jam_2/D2Jam_2.Build.cs
@@ -8,7 +8,7 @@ public class D2Jam_2 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "TrickyProperties", "TrickyGameplayObjects", "TrickyGameMode" });
 
