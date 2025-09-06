9f01b9a6df774f705e831ea0660dd8151817dfb5
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-09-06 11:10:50 +0300

    Create star pickup

---
+++ Source/D2Jam_2/GameplayObjects/Pickups/D2JStarPickup.cpp
@@ -0,0 +1,87 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "D2JStarPickup.h"
+
+#include "Components/SphereComponent.h"
+#include "D2Jam_2/PlayerCharacter/D2JPlayerInterface.h"
+#include "GameplayObject/GameplayObjectStateControllerComponent.h"
+
+
+AD2JStarPickup::AD2JStarPickup()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	MeshComponent->SetupAttachment(GetRootComponent());
+	MeshComponent->SetCollisionProfileName(TEXT("NoCollision"));
+
+	StateControllerComponent = CreateDefaultSubobject<UGameplayObjectStateControllerComponent>(TEXT("StateController"));
+}
+
+#if WITH_EDITOR
+void AD2JStarPickup::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	const EGameplayObjectState InitialState = bIsActiveOnStart
+		                                          ? EGameplayObjectState::Active
+		                                          : EGameplayObjectState::Disabled;
+	StateControllerComponent->SetInitialState(InitialState);
+
+	const ECollisionEnabled::Type CollisionType = bIsActiveOnStart
+		                                              ? ECollisionEnabled::NoCollision
+		                                              : ECollisionEnabled::QueryOnly;
+	ActivationTrigger->SetCollisionEnabled(CollisionType);
+
+	MeshComponent->SetHiddenInGame(!bIsActiveOnStart);
+}
+#endif
+
+void AD2JStarPickup::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	if (IsValid(StateControllerComponent))
+	{
+		StateControllerComponent->OnGameplayObjectStateChanged.AddDynamic(this, &AD2JStarPickup::HandleStateChanged);
+	}
+}
+
+bool AD2JStarPickup::ActivateGameplayObject_Implementation(const bool bTransitImmediately)
+{
+	return Execute_ActivateGameplayObject(StateControllerComponent, bTransitImmediately);
+}
+
+bool AD2JStarPickup::DeactivateGameplayObject_Implementation(const bool bTransitImmediately)
+{
+	return Execute_DeactivateGameplayObject(StateControllerComponent, bTransitImmediately);
+}
+
+bool AD2JStarPickup::CanBeActivated_Implementation(AActor* Activator)
+{
+	return Activator->GetClass()->ImplementsInterface(UD2JPlayerInterface::StaticClass());
+}
+
+void AD2JStarPickup::HandleActivationSuccess_Implementation(AActor* Activator)
+{
+	Cast<ID2JPlayerInterface>(Activator)->AddStar();
+}
+
+void AD2JStarPickup::HandleStateChanged(UGameplayObjectStateControllerComponent* Component,
+                                        EGameplayObjectState NewState,
+                                        bool bChangedImmediately)
+{
+	switch (NewState)
+	{
+	case EGameplayObjectState::Active:
+		MeshComponent->SetHiddenInGame(false);
+		ActivationTrigger->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+		break;
+
+	case EGameplayObjectState::Inactive:
+		MeshComponent->SetHiddenInGame(true);
+		ActivationTrigger->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		break;
+	}
+}
---
+++ Source/D2Jam_2/GameplayObjects/Pickups/D2JStarPickup.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayObject/GameplayObjectInterface.h"
+#include "Pickup/PickupAutomaticBase.h"
+#include "D2JStarPickup.generated.h"
+
+class UGameplayObjectStateControllerComponent;
+
+UCLASS()
+class D2JAM_2_API AD2JStarPickup : public APickupAutomaticBase, public IGameplayObjectInterface
+{
+	GENERATED_BODY()
+
+public:
+	AD2JStarPickup();
+
+protected:
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+	virtual void PostInitializeComponents() override;
+
+public:
+	virtual bool ActivateGameplayObject_Implementation(const bool bTransitImmediately) override;
+
+	virtual bool DeactivateGameplayObject_Implementation(const bool bTransitImmediately) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UStaticMeshComponent> MeshComponent = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category="Components")
+	TObjectPtr<UGameplayObjectStateControllerComponent> StateControllerComponent = nullptr;
+
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category="Pickup")
+	bool bIsActiveOnStart = true;
+
+	virtual bool CanBeActivated_Implementation(AActor* Activator) override;
+
+	virtual void HandleActivationSuccess_Implementation(AActor* Activator) override;
+
+	UFUNCTION()
+	void HandleStateChanged(UGameplayObjectStateControllerComponent* Component,
+	                        EGameplayObjectState NewState,
+	                        bool bChangedImmediately);
+};
