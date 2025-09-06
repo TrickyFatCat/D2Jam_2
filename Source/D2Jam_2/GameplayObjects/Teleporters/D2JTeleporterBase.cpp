af06f040796e00d83d200ab375d1602446ebbb93
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-09-06 16:33:53 +0300

    Create base teleporter class

---
+++ Source/D2Jam_2/GameplayObjects/Teleporters/D2JTeleporterBase.cpp
@@ -0,0 +1,69 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "D2JTeleporterBase.h"
+
+#include "Components/SphereComponent.h"
+#include "D2Jam_2/PlayerCharacter/D2JPlayerInterface.h"
+#include "Kismet/GameplayStatics.h"
+
+
+AD2JTeleporterBase::AD2JTeleporterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	ActivationTrigger = CreateDefaultSubobject<USphereComponent>(TEXT("ActivationTrigger"));
+	ActivationTrigger->SetupAttachment(GetRootComponent());
+	ActivationTrigger->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	ActivationTrigger->SetCollisionResponseToAllChannels(ECR_Ignore);
+	ActivationTrigger->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECR_Overlap);
+}
+
+void AD2JTeleporterBase::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	if (GetWorld() && GetWorld()->IsGameWorld())
+	{
+		ActivationTrigger->OnComponentBeginOverlap.AddUniqueDynamic(this,
+		                                                            &AD2JTeleporterBase::HandleTriggerBeginOverlap);
+	}
+}
+
+void AD2JTeleporterBase::HandleTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+                                                   AActor* OtherActor,
+                                                   UPrimitiveComponent* OtherComp,
+                                                   int32 OtherBodyIndex,
+                                                   bool bFromSweep,
+                                                   const FHitResult& SweepResult)
+{
+	if (!OtherActor->GetClass()->ImplementsInterface(UD2JPlayerInterface::StaticClass()))
+	{
+		return;
+	}
+
+	GetWorldTimerManager().SetTimer(ActivationTimerHandle,
+	                                this,
+	                                &AD2JTeleporterBase::HandleActivationTimerFinished,
+	                                ActivationDelay);
+}
+
+void AD2JTeleporterBase::HandleActivationTimerFinished()
+{
+	APlayerCameraManager* CameraManager = UGameplayStatics::GetPlayerCameraManager(this, 0);
+
+	if (IsValid(CameraManager))
+	{
+		CameraManager->StartCameraFade(0.f, 1.f, CameraFadeInDuration, FLinearColor::Black, true, true);
+		FTimerHandle TimerHandle;
+		GetWorldTimerManager().SetTimer(TimerHandle,
+		                                this,
+		                                &AD2JTeleporterBase::HandleCameraFadeInFinished,
+		                                CameraFadeInDuration,
+		                                false);
+	}
+}
+
+void AD2JTeleporterBase::HandleCameraFadeInFinished()
+{
+}
---
+++ Source/D2Jam_2/GameplayObjects/Teleporters/D2JTeleporterBase.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "D2Jam_2/GameplayObjects/D2JGameplayObjectBase.h"
+#include "D2JTeleporterBase.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class D2JAM_2_API AD2JTeleporterBase : public AD2JGameplayObjectBase
+{
+	GENERATED_BODY()
+
+public:
+	AD2JTeleporterBase();
+
+	virtual void PostInitializeComponents() override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Component")
+	TObjectPtr<USphereComponent> ActivationTrigger = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Teleporter")
+	float ActivationDelay = 3.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Teleporter")
+	float CameraFadeInDuration = 0.5f;
+
+	UPROPERTY(BlueprintReadOnly, Category="Teleporter")
+	FTimerHandle ActivationTimerHandle;
+
+	UFUNCTION()
+	virtual void HandleTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+	                                       AActor* OtherActor,
+	                                       UPrimitiveComponent* OtherComp,
+	                                       int32 OtherBodyIndex,
+	                                       bool bFromSweep,
+	                                       const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void HandleActivationTimerFinished();
+
+	UFUNCTION()
+	virtual void HandleCameraFadeInFinished();
+};
