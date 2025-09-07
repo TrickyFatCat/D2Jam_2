758d09b57eeb8f2ef025e67b912a4b28737027ab
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-09-07 18:59:37 +0300

    Create utility library

---
+++ Source/D2Jam_2/Core/D2JUtils.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "D2JUtils.h"
+
+#include "D2JGameInstance.h"
+#include "Kismet/GameplayStatics.h"
+
+UD2JGameInstance* UD2JUtils::GetJamInstance(UObject* WorldContextObject)
+{
+	return Cast<UD2JGameInstance>(UGameplayStatics::GetGameInstance(WorldContextObject));
+}
+
+bool UD2JUtils::GetLevelSaveData(UObject* WorldContextObject, const FString& LevelName, FLevelData& OutLevelData)
+{
+	if (!IsValid(WorldContextObject))
+	{
+		return false;
+	}
+
+	UD2JGameInstance* JamInstance = GetJamInstance(WorldContextObject);
+
+	if (!IsValid(JamInstance))
+	{
+		return false;
+	}
+
+	TArray<FLevelData> LevelData;
+	JamInstance->GetLevelData(LevelData);
+
+	if (LevelData.IsEmpty())
+	{
+		return false;
+	}
+
+	auto Predicate = [LevelName](const FLevelData& Data) { return Data.LevelName == LevelName; };
+	const FLevelData* TargetLevelData = LevelData.FindByPredicate(Predicate);
+
+	if (!TargetLevelData)
+	{
+		return false;
+	}
+	OutLevelData = *TargetLevelData;
+	return true;
+}
